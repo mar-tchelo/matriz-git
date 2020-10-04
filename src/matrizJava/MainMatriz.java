697a7845c9158211a96780b618b09ede9cdfa9e8
mar-tchelo <marcelo.js10@gmail.com>
2020-10-04 16:55:33 -0300

    Projeto criado

---
+++ src/matrizJava/MainMatriz.java
@@ -0,0 +1,39 @@
+package matrizJava;
+
+import java.util.Scanner;
+
+public class MainMatriz {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int[][] mat = new int[n][n];
+
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat.length; j++) {
+				mat[i][j] = sc.nextInt();
+			}
+		}
+
+		System.out.println("Main diagnal: ");
+		for (int i = 0; i < n; i++) {
+			System.out.print(mat[i][i] + " ");
+		}
+		
+		System.out.println();
+		int count = 0;		
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat.length; j++) {
+				if (mat[i][j] < 0) {
+					 count++;
+				}
+			}
+		}
+		System.out.print("Negative numbers = " + count);
+		
+		sc.close();
+	}
+
+}
